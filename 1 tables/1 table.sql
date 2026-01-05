8a530c1a7dcd0df52ecfe7b131ffcbe84a81b640
rachelih10 <rachelihoro613@gmail.com>
2026-01-05 18:02:22 -0500

    Update 1 table.sql
    added planet to the body table

--- 1 tables/1 table.sql
+++ 1 tables/1 table.sql
@@ -3,5 +3,7 @@ go
 drop table if exists body
 go
 create table dbo.body(id int not null identity primary key, 
-bodypart varchar(100) not null,
- num int not null)
+ planet varchar(50) not null
+ bodypart varchar(100) not null,
+ num int not null
+ )
